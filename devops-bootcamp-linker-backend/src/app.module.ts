02ce41407052581b7e43458991f236343cc71d8d
obianuju-linda <obianujuowoh15@gmail.com>
2026-06-10 12:47:55 +0100

    Update Docker and health check configuration

--- devops-bootcamp-linker-backend/src/app.module.ts
+++ devops-bootcamp-linker-backend/src/app.module.ts
@@ -5,11 +5,17 @@ import { LinksModule } from "./links/links.module";
 import { UploadModule } from "./upload/upload.module";
 import { PrismaService } from "./prisma";
 import { JwtService } from "./common/jwt.service";
+import { HealthController } from "./health.controller";
 
 @Module({
-  imports: [AuthModule, ProfileModule, LinksModule, UploadModule],
+  imports: [
+    AuthModule,
+    ProfileModule,
+    LinksModule,
+    UploadModule,
+  ],
+  controllers: [HealthController],
   providers: [PrismaService, JwtService],
   exports: [PrismaService, JwtService],
 })
 export class AppModule {}
-
